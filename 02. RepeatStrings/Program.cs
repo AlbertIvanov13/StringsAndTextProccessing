5b94f501038ff3e69affcd621de23a737cebcc91
Albert Ivanov <albertivanov187@gmail.com>
2025-01-08 17:32:42 +0200

    100/100 :D

---
+++ 02. RepeatStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string[] words = Console.ReadLine().Split().ToArray();
+
+List<string> result = new List<string>();
+for (int i = 0; i < words.Length; i++)
+{
+	string word = words[i];
+	for (int j = 0; j < word.Length; j++)
+	{
+		result.Add(word);
+	}
+}
+
+Console.WriteLine(string.Join("", result));
