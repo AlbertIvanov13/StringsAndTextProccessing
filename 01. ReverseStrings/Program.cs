7d21d96e23c2d6fed18fcd58b74ffcdaddc62aea
Albert Ivanov <albertivanov187@gmail.com>
2025-01-08 17:16:53 +0200

    Add project files.

---
+++ 01. ReverseStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿
+while(true)
+{
+	string word = Console.ReadLine();
+	if (word == "end")
+	{
+		break;
+	}
+
+	List<string> characters = new List<string>();
+	for (int i = 0; i < word.Length; i++)
+	{
+		characters.Add(word[i].ToString());
+	}
+
+	characters.Reverse();
+
+	Console.WriteLine($"{word} = {string.Join("", characters)}");
+}
