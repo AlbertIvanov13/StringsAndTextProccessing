b98084900108087c0318cffd170a8d77e8e0bc5a
Albert Ivanov <albertivanov187@gmail.com>
2025-01-09 15:53:18 +0200

    100/100

---
+++ 03. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string firstWord = Console.ReadLine();
+string secondWord = Console.ReadLine();
+
+
+for (int i = 0; i < secondWord.Length; i++)
+{
+	if (secondWord.Contains(firstWord))
+	{
+		int index = secondWord.IndexOf(firstWord);
+		secondWord = secondWord.Remove(index, firstWord.Length);
+		continue;
+	}
+	else
+	{
+		break;
+	}
+}
+
+Console.WriteLine(secondWord);
