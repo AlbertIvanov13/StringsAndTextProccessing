e10e9c143039a685a4c41010cef45356d242c3b9
Albert Ivanov <albertivanov187@gmail.com>
2025-01-09 23:26:31 +0200

    Update

---
+++ 04. TextFilter/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string bannedWords = Console.ReadLine();
+string text = Console.ReadLine();
+
+for (int i = 0; i < bannedWords.Length; i++)
+{
+	foreach (var word in text)
+	{
+		if (word == bannedWords[i])
+		{
+
+		}
+	}
+}
