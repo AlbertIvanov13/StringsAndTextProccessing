ca05ba59419801de157af1f1762a6a3022468470
Albert Ivanov <albertivanov187@gmail.com>
2025-01-10 00:24:25 +0200

    100/100

---
+++ 05. Digits,LettersAndOthers/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string input = Console.ReadLine();
+
+List<char> digits = new List<char>();
+List<char> letters = new List<char>();
+List<char> characters = new List<char>();
+
+for (int i = 0; i < input.ToString().Length; i++)
+{
+	if (char.IsDigit(input.ToString()[i]))
+	{
+		digits.Add(input.ToString()[i]);
+	}
+	else if (char.IsLetter(input.ToString()[i]))
+	{
+		letters.Add(input.ToString()[i]);
+	}
+	else if (char.IsAscii(input[i]))
+	{
+		characters.Add(input.ToString()[i]);
+	}
+}
+
+Console.WriteLine(string.Join("", digits));
+Console.WriteLine(string.Join("", letters));
+Console.WriteLine(string.Join("", characters));
