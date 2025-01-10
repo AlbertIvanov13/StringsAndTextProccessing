de0fe038ee1df4bef23824203060be35b7a0505e
Albert Ivanov <albertivanov187@gmail.com>
2025-01-10 16:37:41 +0200

    100/100

---
+++ 06. ValidUsernames/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Specialized;
+
+string[] usernames = Console.ReadLine().Split(", ");
+
+List<string> validNames = new List<string>();
+
+foreach (string username in usernames)
+{
+	if (username.Length < 3 || username.Length > 16)
+	{
+		continue;
+	}
+
+	bool isValid = username.All(symbol => char.IsLetterOrDigit(symbol) || symbol == '-' || symbol == '_');
+
+	if (isValid)
+	{
+		validNames.Add(username);
+	}
+}
+
+foreach (string name in validNames)
+{
+    Console.WriteLine(name);
+}
