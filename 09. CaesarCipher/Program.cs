e737e81d515f3277ac3c7a803babcf60986b474e
Albert Ivanov <albertivanov187@gmail.com>
2025-01-11 01:32:17 +0200

    100/100 :D

---
+++ 09. CaesarCipher/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string text = Console.ReadLine();
+
+List<string> encryptedText = new List<string>();
+
+for (int i = 0; i < text.Length; i++)
+{
+	int upper = Convert.ToInt32(text[i]);
+	int newChar = upper + 3;
+	char newerChar = Convert.ToChar(newChar);
+	encryptedText.Add(newerChar.ToString());
+}
+
+Console.WriteLine(string.Join("", encryptedText));
