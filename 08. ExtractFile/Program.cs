3e6dec7101af5c7e897ada0db1df3b9c38ca57ec
Albert Ivanov <albertivanov187@gmail.com>
2025-01-10 23:39:02 +0200

    Update

---
+++ 08. ExtractFile/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string[] path = Console.ReadLine().Split(new char[] { ':', '\\', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+string disk = path[0];
+string folderName = path[1];
+string subfolderName = path[2];
+string fileName = path[3];
+string fileExtension  = path[4];
+
+Console.WriteLine($"File name: {fileName}");
+Console.WriteLine($"File extension: {fileExtension}");
