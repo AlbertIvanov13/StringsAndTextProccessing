1df76817ce96fd84b74d9293df60c0268987bd5c
Albert Ivanov <albertivanov187@gmail.com>
2025-01-12 23:58:10 +0200

    Update

---
+++ 12.  StringExplosion/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] text = Console.ReadLine().Split(">");
+
+for (int i = 0; i < text.Length; i++)
+{
+	for (int j = 0; j < text[i].Length; j++)
+	{
+		if (char.IsDigit(text[i][j]))
+		{
+			if (text[i].Length >= 1)
+			{
+				text[i].Remove(j);
+			}
+			if (text[i].Length > 1)
+			{
+				if (char.IsLetter(text[i][j + 1]))
+				{
+					string newText = text[i].Substring(j + 1);
+				}
+			}
+		}
+	}
+}
