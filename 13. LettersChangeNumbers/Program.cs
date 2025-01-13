cfa47364e83fdbd1e19a52b7e854915e413fa567
Albert Ivanov <albertivanov187@gmail.com>
2025-01-13 23:46:12 +0200

    Update

--- 13. LettersChangeNumbers/Program.cs
+++ 13. LettersChangeNumbers/Program.cs
@@ -3,6 +3,7 @@ string[] inputs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEn
 
 int totalSum = 0;
 string newNumber = "";
+int number = 0;
 
 for (int i = 0; i < inputs.Length; i++)
 {
@@ -24,25 +25,20 @@ for (int i = 0; i < inputs.Length; i++)
 				{
 					continue;
 				}
-				int number = int.Parse(newNumber);
+				number = int.Parse(newNumber);
 				char letterPositionn = char.Parse(inputs[i][j].ToString());
 				int letterPosition = letterPositionn;
 				int result = letterPosition - 'A' + 1;
 				totalSum += number / result;
+				break;
 			}
 		}
 		else if (char.IsLower(inputs[i][j]))
 		{
-
-		}
-
-		if (char.IsUpper(inputs[i][inputs[i].Length - 1]))
-		{
-
-		}
-		else if (char.IsLower(inputs[i][inputs[i].Length - 1]))
-		{
-
+			char letterPositionn = char.Parse(inputs[i][j].ToString());
+			int letterPosition = letterPositionn;
+			int result = char.ToUpper(letterPositionn) - 'A' + 1;
+			totalSum += result;
 		}
 	}
 }
